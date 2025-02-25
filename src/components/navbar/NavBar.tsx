1985fe97bd4ae13fee1df13e2a960a65cd183240
Matas <matas.lankelis@gmail.com>
2025-02-26 01:04:50 +0200

    Updated NavBar design

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -6,6 +6,27 @@ function NavBar() {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
     <nav>
+      <Link to="/" className="home-link">
+        Home
+      </Link>
+
+      <div className={`search-container ${isExpanded ? "expanded" : ""}`}>
+        <button
+          className="search-icon"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 -960 960 960"
+            width="20"
+          >
+            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+          </svg>
+        </button>
+        <input type="text" placeholder="Search..." />
+      </div>
+
       <input type="checkbox" id="sidebar-active" />
       <label htmlFor="sidebar-active" className="open-sidebar-button">
         <svg
@@ -19,7 +40,6 @@ function NavBar() {
       </label>
       <label id="overlay" htmlFor="sidebar-active"></label>
 
-      <button className={"btn btn-outline-primary"}>button</button>
       <div className="links-container">
         <label htmlFor="sidebar-active" className="close-sidebar-button">
           <svg
@@ -31,27 +51,6 @@ function NavBar() {
             <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
           </svg>
         </label>
-
-        <Link to="/" className="home-link">
-          Home
-        </Link>
-
-        <div className={`search-container ${isExpanded ? "expanded" : ""}`}>
-          <button
-            className="search-icon"
-            onClick={() => setIsExpanded(!isExpanded)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 -960 960 960"
-              width="20"
-            >
-              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-            </svg>
-          </button>
-          <input type="text" placeholder="Search..." />
-        </div>
 
         <Link to="/Store">Store</Link>
         <Link to="/Championships">Championships</Link>
