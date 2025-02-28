bd09a787030480fa2cc9c7a3075685f694caa1e1
M35KA <arnas.penkauskas@gmail.com>
2025-02-28 19:14:07 +0200

    Cleaned up the workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Register from "./pages/Register.tsx";
 import LogIn from "./pages/LogIn.tsx";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,7 +10,6 @@ function App() {
       <div>
         <main>
           <Routes>
-            <Route path="/Register" element={<Register />} />
             <Route path="/Log In" element={<LogIn />} />
             <Route path="/" element={<Home />} />
           </Routes>
