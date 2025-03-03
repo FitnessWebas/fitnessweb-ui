49c0f1156dcef3f0afdbcf9ab47dd3ee0d4b738a
Kipras <kipraskb@gmail.com>
2025-03-03 19:05:01 +0200

    Created Files for the Profile Setup

---
+++ src/components/SetupForm/SetupForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SetupForm() {
+  return <div>SetupForm</div>;
+}
---
+++ src/pages/Setup.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import SetupForm from "../components/SetupForm/SetupForm";
+
+function Setup() {
+  return (
+    <div className="">
+      <SetupForm />
+    </div>
+  );
+}
+
+export default Setup;
