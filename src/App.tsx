179c743b19716f159d01923940f7f5050eeff4be
Kipras <kipraskb@gmail.com>
2025-03-03 19:47:56 +0200

    Created main components and route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn.tsx";
+import Setup from "./pages/Setup.tsx";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,6 +12,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/LogIn" element={<LogIn />} />
+            <Route path="/Setup" element={<Setup />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </main>
--- src/components/SetupForm/SetupForm.tsx
+++ src/components/SetupForm/SetupForm.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
 export default function SetupForm() {
-  return <div>SetupForm</div>;
+  return (
+    <form className="setup">
+      <div className="setup-gender">
+        <button>Male</button>
+        <button>Female</button>
+      </div>
+      <div className="setup-age">
+        <input type="number" min={1} max={110} />
+      </div>
+      <div className="setup-weight">
+        <input type="number" min={0} />
+      </div>
+      <div className="setup-goals">
+        <input type="checkbox" />
+        <label htmlFor=""></label>
+        <input type="checkbox" />
+        <label htmlFor=""></label>
+        <input type="checkbox" />
+        <label htmlFor=""></label>
+      </div>
+      <div className="setup-level">
+        <select name="" id="">
+          <option value="">--Choose an option--</option>
+          <option value="">Begginer</option>
+          <option value="">Intermediate</option>
+          <option value="">Expert</option>
+        </select>
+        <div className="setup-"></div>
+      </div>
+      <div className="setup-equipment">
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+      </div>
+    </form>
+  );
 }
