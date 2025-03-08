2f5e0d6a3c340cef8868087e5fe62611a4e17651
M35KA <arnas.penkauskas@gmail.com>
2025-03-08 21:49:55 +0200

    Removed leftover imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import ExerciseDirectory from "./pages/ExerciseDirectory";
 import LogIn from "./pages/LogIn.tsx";
-
 import Setup from "./pages/Generator.tsx";
 import Home from "./pages/Home";
-import Register from "./pages/Register.tsx";
-
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -12,14 +9,12 @@ import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-
     <div>
       <NavBar />
       <main style={{ paddingTop: "70px" }}>
         <Routes>
           <Route path="/LogIn" element={<LogIn />} />
           <Route path="/" element={<Home />} />
-          <Route path="/Register" element={<Register />} />
           <Route path="/Exercises/Directory" element={<ExerciseDirectory />} />
           <Route path="/Workouts/Generator" element={<Setup />} />
         </Routes>
