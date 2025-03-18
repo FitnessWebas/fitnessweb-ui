db2797494dd9531009da538409032806543501ee
Kipras <kipraskb@gmail.com>
2025-03-18 23:29:49 +0200

    created header for workout page and added some styling

--- src/components/WorkoutForms/WorkoutForms.tsx
+++ src/components/WorkoutForms/WorkoutForms.tsx
@@ -6,7 +6,20 @@ import { h1, h2 } from "framer-motion/client";
 const WorkoutForms = () => {
   return (
     <div className={styles.container}>
-      <WorkoutForm />
+      <div className={styles.header}>
+        <h1>
+          My <span>Workouts</span>
+        </h1>
+        <hr />
+      </div>
+      <div className={styles.forms}>
+        <div className={styles.filter}></div>
+        <div className={styles.workoutForms}>
+          <WorkoutForm />
+          <WorkoutForm />
+          <WorkoutForm />
+        </div>
+      </div>
     </div>
   );
 };
