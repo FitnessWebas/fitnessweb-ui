2aa9f6d68ce06501b2ab4671fe315cf720e70022
Kipras <kipraskb@gmail.com>
2025-03-18 12:22:44 +0200

    Created intial files for the feature

---
+++ src/pages/Workouts.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import WorkoutForm from "../components/WorkoutForm/WorkoutForm";
+
+function Setup() {
+  return (
+    <div className="vh-100 d-flex justify-content-center">
+      <WorkoutForm />
+    </div>
+  );
+}
+
+export default Setup;
