58081428c8c4e52fa1620eccab6834aad43a0359
Kipras <kipraskb@gmail.com>
2025-03-18 12:07:18 +0200

    Creted intial components for wokouts page

---
+++ src/pages/Workouts.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WorkoutsForm from "../components/WorkoutForn/WorkoutForm";
+
+export const Workouts = () => {
+  return (
+    <div className="vh-100 d-flex justify-content-center">
+      <WorkoutsForm />
+    </div>
+  );
+};
